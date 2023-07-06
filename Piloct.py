be7ad92e8e173f4b293a14fde6bf044881b59dda
Raffaele15 <127408973+VSRaffaele15@users.noreply.github.com>
2023-07-06 10:54:50 +0200

    HighLow Change

---
+++ Piloct.py
@@ -0,0 +1,22 @@
+def GravityForce(
+        force: float
+        ):
+    return force * 8002
+
+def JumpForce(
+        force: float
+        ):
+    return force * 2
+
+def High(
+        a: float,
+        b: float
+        ):
+    return a + 1 == b
+
+def Low(
+        a: float,
+        b: float
+        ):
+    return a - 1 == b
+
